8b615b1ba35ba8108364fba5b5c5acc05846fb3f
naoyamaguchi <naoyamaguchi0128@gmail.com>
2018-04-16 17:27:40 +0900

    edit

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ var ASCIISTR = "MND8OZ$7I?+=~:,.."
 func Init() (image.Image, string, int, int, string, string) {
 	// width := flag.Int("w", 30, "Use -w <width>")
 	// height := flag.Int("h", 30, "Use -w <height>")
-	inputfulename := flag.String("input", "test.png", "Use -input <filesource>")
+	inputfilename := flag.String("input", "test.png", "Use -input <filesource>")
 	outputformat := flag.String("out", "jpg", "Use -out <output file format>")
 	flag.Parse()
 
-	f, err := os.Open(*inputfulename)
+	f, err := os.Open(*inputfilename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,12 +32,12 @@ func Init() (image.Image, string, int, int, string, string) {
 		panic(err)
 	}
 
-	f, err = os.Open(*inputfulename)
+	f, err = os.Open(*inputfilename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	conf, encode, err := image.DecodeConfig(f)
+	conf, inputformat, err := image.DecodeConfig(f)
 	if err != nil {
 		panic(err)
 	}
@@ -45,11 +45,11 @@ func Init() (image.Image, string, int, int, string, string) {
 	// サイズ表示
 	fmt.Printf("INPUT Image Size width: %v px, height: %v px\n", conf.Width, conf.Height)
 	// フォーマット名表示
-	fmt.Printf("INPUT Image Format: %v\n", encode)
+	fmt.Printf("INPUT Image Format: %v\n", inputformat)
 
 	defer f.Close()
 	// return img, *width, *height, *outputformat, encode
-	return img, *inputfulename, conf.Width, conf.Height, *outputformat, encode
+	return img, *inputfilename, conf.Width, conf.Height, *outputformat, inputformat
 }
 
 func Convert2Ascii(img image.Image, w, h int) []byte {
@@ -68,15 +68,22 @@ func Convert2Ascii(img image.Image, w, h int) []byte {
 	return buf.Bytes()
 }
 
-func ConvertFomat(img image.Image, w, h int) []byte {
+func ConvertFomat(img image.Image, outputformat, inputformat, inputfilename string, w, h int) []byte {
 	buf := new(bytes.Buffer)
+
+	// switch strings.ToLower(outputformat) {
+	// case ".png":
+	// 	err = png.Encode(df, img)
+	// case ".jpeg", ".jpg":
+	// 	err = jpeg.Encode(df, img, &jpeg.Options{jpeg.DefaultQuality})
+	// }
 
 	return buf.Bytes()
 }
 
 func main() {
-	img, inputfulename, width, height, outputformat, encode := Init()
-	fmt.Printf("inputfulename: %v\n", inputfulename)
+	img, inputfilename, width, height, outputformat, encode := Init()
+	fmt.Printf("inputfilename: %v\n", inputfilename)
 	fmt.Printf("inputformat: %v\n", encode)
 	fmt.Printf("outputformat: %v\n", outputformat)
 
