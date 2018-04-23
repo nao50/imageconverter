550e0936910b7c3d9cdf2c1550c56a6d14e3c8e9
naoyamaguchi <naoyamaguchi0128@gmail.com>
2018-04-23 18:35:53 +0900

    edit

--- imgconv/imgconv_test.go
+++ imgconv/imgconv_test.go
@@ -20,13 +20,25 @@ func TestNewImages(t *testing.T) {
 }
 
 func TestImgconv(t *testing.T) {
+	t.Helper()
+
 	filepath, image, err := NewImages("../images")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = Imgconv("jpg", filepath, image)
-	if err != nil {
-		t.Fatal("fail error func test")
+	outType := []struct {
+		outtype string
+	}{
+		{outtype: "png"},
+		{outtype: "jpg"},
+	}
+
+	for _, c := range outType {
+
+		err = Imgconv(c.outtype, filepath, image)
+		if err != nil {
+			t.Fatal("fail error func test")
+		}
 	}
 }
